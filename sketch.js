b2ffa7313a8901437ec151f3a8db528294173a14
kaleissmith121 <kaleissmith92@gmail.com>
2026-03-16 22:49:12 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,55 +1,44 @@
-var chunky=45;
+let shapeSize = 50;
 
 function setup() {
-	createCanvas(500, 500); //create a 500px X 500px canvas
+  createCanvas(500, 500);
 }
 
 function draw() {
-  background(mouseX,170,mouseY) ; //an RGB color for the canvas' background
-  
-  stroke(100,100,100);
+  let redVal = map(sin(frameCount * 0.05), -1, 1, 100, 255);
+  let blueVal = map(cos(frameCount * 0.05), -1, 1, 100, 255);
+  background(redVal, 170, blueVal); 
+
+
+  let jump = sin(frameCount * 0.1) * 30;
+
+  let pulse = 50 + sin(frameCount * 0.08) * 10;
+
+  // Dance Floor
+  stroke(100);
   strokeWeight(10);
-  fill(250,250,250);
-  rect(90,350,300,300); //dance floor
+  fill(250);
+  rect(90, 350, 300, 300);
 
-  stroke(255,255,100);
+  stroke(255, 255, 100);
   strokeWeight(5);
-  fill(19,109,21);
-  ellipse(240,450,chunky); //main alien
+  fill(19, 109, 21);
 
-  stroke(255,255,100);
-  strokeWeight(5);
-  fill(19,109,21);
-  ellipse(145,385,chunky); //alien 1
+  ellipse(240, 450 + jump, pulse); // Main alien
+  ellipse(145, 385 + jump, pulse); // Alien 1
+  ellipse(215, 385 + jump, pulse); // Alien 2
+  ellipse(270, 385 + jump, pulse); // Alien 3
+  ellipse(330, 385 + jump, pulse); // Alien 4
 
-  stroke(255,255,100);
-  strokeWeight(5);
-  fill(19,109,21);
-  ellipse(215,385,chunky); //alien 2
+  // Disco Ball
+  stroke(100);
+  fill(50);
+  ellipse(225, 117, 125); 
+  rect(215, 2, 20, 50); // stick
 
-  stroke(255,255,100);
-  strokeWeight(5);
-  fill(19,109,21);
-  ellipse(270,385,chunky); //alien 3
-
-  stroke(255,255,100);
-  strokeWeight(5);
-  fill(19,109,21);
-  ellipse(330,385,chunky); //alien 4
-
-  stroke(100,100,100);
-  strokeWeight(10);
-  fill(50,50,50);
-  ellipse(225,117,125); //disco ball
-  
-  stroke(0,0,0);
-  strokeWeight(5);
-  fill(27,27,27);
-  rect(215,2,20,50); //disco stick
-
-  stroke(255,0,255); //Fuchsia colored text outline
-  strokeWeight(10);
+  // Text
+  stroke(255, 0, 255);
   textFont('Courier');
   textSize(30);
-  text('Planet Bopi Dance Party',25,75);
+  text('Planet Bopi Dance Party', 25, 75);
 }
